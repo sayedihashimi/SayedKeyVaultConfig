16a724d9b63a47898c46dcd1077be461729583c1
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-11-02 19:04:41 -0800

    adding provider

---
+++ src/SayedKeyVaultConfig/KeyUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace SayedKeyVaultConfig
+{
+    public class KeyUtils {
+        //this is an optional property to hold the secret after it is retrieved
+        public static string EncryptSecret { get; set; }
+
+        //the method that will be provided to the KeyVaultClient
+        public async static Task<string> GetToken(string authority, string resource, string scope) {
+            // TODO: Where to get these from?
+            string clientid = Environment.GetEnvironmentVariable("ClientId");
+            string clientsecret = Environment.GetEnvironmentVariable("ClientSecret");
+
+            if (string.IsNullOrWhiteSpace(clientid)) {
+                throw new InvalidOperationException("Did not find required env var ClientId");
+            }
+            if (string.IsNullOrWhiteSpace(clientsecret)) {
+                throw new InvalidOperationException("Did not find required env var ClientSecret");
+            }
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(clientid, clientsecret);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
+        }
+    }
+}
---
+++ src/SayedKeyVaultConfig/KeyVaultConfigurationProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Framework.Configuration;
+using Microsoft.Azure.KeyVault;
+
+namespace SayedKeyVaultConfig {
+    public class KeyVaultConfigurationProvider : ConfigurationProvider {
+
+        private string KeyVaultUri { get; }
+        private string ClientId { get; }
+        private string ClientSecret { get; }
+        private KeyVaultClient VaultClient { get; }
+
+
+        public KeyVaultConfigurationProvider(string keyVaultUri, string clientId, string clientSecret) {
+            if (string.IsNullOrEmpty(keyVaultUri)) { throw new ArgumentNullException(nameof(keyVaultUri)); }
+
+            if (string.IsNullOrWhiteSpace(clientId)) {
+                clientId = Environment.GetEnvironmentVariable("ClientId");
+            }
+            if (string.IsNullOrWhiteSpace(clientSecret)) {
+                clientSecret = Environment.GetEnvironmentVariable("ClientSecret");
+            }
+
+
+            if (string.IsNullOrWhiteSpace("clientId")) {
+                throw new InvalidOperationException("Did not find required env var ClientId");
+            }
+            if (string.IsNullOrWhiteSpace(clientSecret)) {
+                throw new InvalidOperationException("Did not find required env var ClientSecret");
+            }
+
+            KeyVaultUri = keyVaultUri;
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            VaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(KeyUtils.GetToken));
+        }
+
+        public override bool TryGet(string key, out string value) {
+            var result = VaultClient.GetSecretAsync(KeyVaultUri, key, null).Result.Value;
+
+            if (!string.IsNullOrWhiteSpace(result)) {
+                value = result;
+                return true;
+            }
+            else {
+                value = null;
+                return false;
+            }
+        }
+    }
+
+    public static class KeyVaultConfigurationExtensions {
+        public static IConfigurationBuilder AddKeyVault(this IConfigurationBuilder configurationBuilder, string keyVaultUri, string clientId, string clientSecret) {
+            if (configurationBuilder == null) { throw new ArgumentNullException(nameof(configurationBuilder)); }
+            if (string.IsNullOrEmpty(keyVaultUri)) { throw new ArgumentNullException(nameof(keyVaultUri)); }
+
+            configurationBuilder.Add(new KeyVaultConfigurationProvider(keyVaultUri, clientId, clientSecret));
+
+            return configurationBuilder;
+        }
+    }
+}
